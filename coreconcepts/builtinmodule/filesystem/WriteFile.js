2519a75da9780441e008e013a2eb134c401ed5f7
swastinuser.email <swastin95.sahoo@gmail.com>
2023-05-17 14:22:28 +0530

    commited

--- coreconcepts/builtinmodule/filesystem/WriteFile.js
+++
@@ -1,9 +0,0 @@
-const fs = require('node:fs');
-var fileName = './Nodejs/coreconcepts/builtinmodule/filesystem/readfile.txt';
-var data = "where are you bro ?";
-fs.writeFile(fileName, data, (err) => {
-    if (err) { throw err; }
-    
-    
-
-});
--- coreconcepts/builtinmodule/path_demo.js
+++
@@ -1,8 +0,0 @@
-const path = require('node:path');
-
-console.log(__dirname);
-console.log(__filename);
-console.log(path.delimiter);
-console.log(path.sep);
-console.log(path.basename(__filename));
-console.log(path.extname(__filename));
